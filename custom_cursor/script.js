1d75877854c54def939cba4c2ab5e00c9943c410
vipin018 <vipinpathak018@gmail.com>
2025-03-18 14:38:24 +0530

    updated

---
+++ custom_cursor/script.js
@@ -0,0 +1,31 @@
+let main = document.querySelector("#main");
+let cursor = document.querySelector("#cursor");
+let imageDiv = document.querySelector("#image");
+
+// Cursor follow effect
+main.addEventListener("mousemove", function (e) {
+    gsap.to(cursor, {
+        x: e.x,
+        y: e.y,
+        duration: 0.5,
+        ease: "circ.out",
+    });
+});
+
+imageDiv.addEventListener("mouseenter", function () {
+    cursor.innerHTML = "👀";
+    
+    gsap.to(cursor, {
+        scale: 5,
+        opacity: 1,
+    });
+}); 
+
+
+imageDiv.addEventListener("mouseleave", function () {
+    cursor.innerHTML = "";
+    cursor.style.color = "#ffffff";
+    gsap.to(cursor, {
+        scale: 1,
+    });
+});
