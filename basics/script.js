84dfa3bdf65795bb5f58886be988c89971722087
vipin018 <vipinpathak018@gmail.com>
2025-03-18 11:37:13 +0530

    updated

--- basics/script.js
+++ basics/script.js
@@ -1,12 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const h1 = document.querySelector("h1");
+const tl = gsap.timeline();
 
-    h1.addEventListener("mouseenter", function() {
-        gsap.to(h1, { duration: 0.5, scale: 1.2, color: "#ff0000" });
-    });
+// tl.to("#box1", {
+//     duration: 1,
+//     x: 1000,
+//     ease: "power2.inOut",
+// });
 
-    h1.addEventListener("mouseleave", function() {
-        gsap.to(h1, { duration: 0.5, scale: 1, color: "#ffffff" });
-    });
+// tl.from("#box2", {
+//     duration: 1,
+//     x: 1000,
+//     ease: "power2.inOut",
+// });
+
+// tl.fromTo("#box3", {
+//     duration: 1,
+//     x: 0,
+//     ease: "power2.inOut",
+// }, {
+//     x: 1000,
+//     rotation: 360,
+//     ease: "power2.inOut",
+// });
+
+tl.from("nav", {
+    duration: 1,
+    opacity: 0,
+    y: -20,
+    ease: "power2.inOut",
 });
 
+tl.from("h2", {
+    duration: 1,
+    opacity: 0,
+    y: -20,
+    ease: "power2.inOut",
+});
+
+tl.from("nav ul a", {
+    duration: 1,
+    opacity: 0,
+    y: -20,
+    ease: "power2.inOut",
+    stagger: 0.2,
+});
+
+tl.from(".text", {
+    duration: 1,
+    scale: 0,
+    opacity: 0,
+    // y: -20,
+    ease: "power2.inOut",
+});
