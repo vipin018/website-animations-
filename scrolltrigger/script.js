c9975172108d3153db1eed3dbc64d84d1ec3872a
vipin018 <vipinpathak018@gmail.com>
2025-03-18 12:34:36 +0530

    updated

--- scrolltrigger/script.js
+++ scrolltrigger/script.js
@@ -12,8 +12,21 @@ gsap.to("#page2 #box", {
         scroller: "body",
         start: "top 90%",
         end: "bottom 100%",
-        markers: true,
+        // markers: true,
         scrub: 5,
         
     },
 });
+
+gsap.to("#page3 h1", {
+    transform: "translateX(-150%)",
+    scrollTrigger: {
+        trigger: "#page3",
+        scroller: "body",
+        start: "top 0%",
+        end: "top -200%",
+        markers: true,
+        scrub: 5,
+        pin:true,
+    }
+})
